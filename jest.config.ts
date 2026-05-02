4d5a12e9de5e744e323c31d0d84b4f04aa5bf318
Alexandre Borela <alexandre@borela.tech>
2026-05-02 19:50:27 -0300

    Upgrade jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type {Config} from 'jest'
 
 const config: Config = {
-  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   transform: {
     '^.+\\.tsx?$': '@swc/jest',
