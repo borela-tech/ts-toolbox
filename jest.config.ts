1c58bfa75c7caccdf56aba6efce7c569c9bfbd94
Alexandre Borela <alexandre@borela.tech>
2026-05-02 18:29:06 -0300

    Adjust jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
 import type {Config} from 'jest'
 
 const config: Config = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   transform: {
     '^.+\\.tsx?$': '@swc/jest',
   },
