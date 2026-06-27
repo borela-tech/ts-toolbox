06a23fff7d96657c24f98b256ffdae85180c8709
Alexandre Borela <alexandre@borela.tech>
2026-06-27 11:56:59 -0300

    Allow functions without abort signal

--- src/debounce/DebouncedFunctionParameters.ts
+++ src/debounce/DebouncedFunctionParameters.ts
@@ -4,4 +4,4 @@ import type {AnyFunction} from '../AnyFunction'
 export type DebouncedFunctionParameters<T extends AnyFunction> =
   Parameters<T> extends [...infer U, AbortSignal]
     ? U
-    : never
+    : Parameters<T>
